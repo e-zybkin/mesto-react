22fb95ba12b16c4414499555526b94c6527bc724
e-zybkin <zybkin.evgeniy@gmail.com>
2022-01-24 16:24:16 +0300

    fix: src img popup

--- src/components/ImagePopup/ImagePopup.js
+++ src/components/ImagePopup/ImagePopup.js
@@ -6,7 +6,7 @@ function ImagePopup(props) {
 			<div className="popup__picture-content">
 				<button type="button" onClick={props.onClose} className="popup__close-btn buttons"></button>
 				<img className="popup__picture-image"
-          src={`${props.card.link}`}
+          src={props.card ? props.card.link : ''}
           alt={props.card.name}
         />
 				<p className="popup__picture-caption">{props.card.name}</p>
